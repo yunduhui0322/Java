2cc5a40e096cb36e6c936a78823d7abdd756c505
yunduhui0322 <wwe3865@gamil.com>
2021-05-18 17:46:14 +0900

    5월18일

--- p0518/src/Ex0518_05.java
+++ p0518/src/Ex0518_05.java
@@ -1,24 +1,24 @@
 import java.util.Scanner;
 
 public class Ex0518_05 {
+	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("두수를 입력하세요");
-
+		
+		Ex0518_05 s = new Ex0518_05();
+		
 		int num1 = scan.nextInt();
 		int num2 = scan.nextInt();
-
-		System.out.println(max(num1, num2));
+		
+		int result =s.add(num1,num2);
+		System.out.println(result);
 
 	}// main method
 
-	static int max(int num1, int num2) {
-		int result = 0;// 결과값
-		if (num1 > num2) {
-			result = num1;
-		} else {
-			result = num2;
-		}
+	//두수를 더해서 리턴해주는 메소드
+		int add(int num1, int num2) {
+		int result = num1 + num2;
 
 		return result;
 	}
---
+++ p0518/src/Test.java
@@ -0,0 +1,10 @@
+
+public class Test {
+
+	
+	int a = 1111111111;
+	int b = 1212121212;
+	
+	Ex0518_05 e = new Ex0518_05();
+	int result = e.add(a,b);
+}
